d3e6c4648c3cfbc5e94aaf2b0ef24edc70543a29
Goran Kovač <you@example.com>
2022-06-20 20:37:36 +0200

    Perlin noise added

--- Assets/Scripts/GenerateTerrain.cs
+++ Assets/Scripts/GenerateTerrain.cs
@@ -12,47 +12,86 @@ public class GenerateTerrain : MonoBehaviour
     public float scale = 20;
     public int radius=10;
     public Vector3 TerrainCenter;
+    Terrain terrain;
+    public float octaves=4;
+    public float lacunarity = 1;//1;
+    public float persistance = 1; //pers 1;
    
+   
+    private void Awake()
+    {
+        _offsetX = Random.Range(-10000, 10000);
+        _offsetY = Random.Range(-10000, 10000);
+    }
     private void Start()
     {
-        Terrain terrain = GetComponent<Terrain>();
+        terrain = GetComponent<Terrain>();
         terrain.terrainData = GenerateTerrainScale(terrain.terrainData);
-
+  
     }
-
+   
 
     TerrainData GenerateTerrainScale(TerrainData TerrainData)
     {
         TerrainData.heightmapResolution = _width + 1;
-        TerrainData.size = new Vector3(_width, _depth, _height);
-        TerrainCenter = TerrainData.bounds.center;
+        TerrainData.size = new Vector3 (_width, _depth, _height);
+        TerrainCenter = TerrainData.bounds.center ;
+        
+        
         int X = (int)TerrainCenter.x;
         int Z = (int)TerrainCenter.z;
-       
-        TerrainData.SetHeights(X,Z,GenerateHeights());
-      
-       
+        
+     
+        TerrainData.SetHeights(X ,Z  , GenerateHeights() );  
         return TerrainData;
     }
-    float[,] GenerateHeights()
+    float[, ] GenerateHeights()
     {
+        float maxNoise = float.MinValue;
+        float MinNoise = float.MaxValue;
+       
+
         float[,] heights = new float[_width, _height];
-        for (int x = 0; x <_width ; x++)
+        for (int y = 0; y < _height ; y++)
         {
-            for (int y = 0; y < _height; y++)
+            for (int x= 0; x < _width; x++)
             {
-                heights[x, y] = CalculateHeight(x, y);
+                float noiseHeight =0;
+                float frequency = 1;
+                float amplitude = 1;
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = x / scale * frequency + _offsetX;
+                    float sampleY = y / scale * frequency + _offsetY;
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+               
+                if (noiseHeight>maxNoise)
+                {
+                    maxNoise = noiseHeight;
+                }
+                else if (noiseHeight<MinNoise)
+                {
+                    MinNoise = noiseHeight;
+                }
+                heights[x, y] = noiseHeight;
+
             }
         }
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+           {
+                heights[x, y] = Mathf.InverseLerp(MinNoise, maxNoise, heights[x, y]);
+            }
+        }
+
         return heights;
     }
-    float CalculateHeight(int x,int y)
-    {
-        float xCoord = (float) x / _width*scale  ;
-        float yCoord = (float) y / _height*scale;
-      
-        return Mathf.PerlinNoise(xCoord, yCoord);
-    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -60,6 +99,7 @@ public class GenerateTerrain : MonoBehaviour
         
      
     }
+   
   
 
 
